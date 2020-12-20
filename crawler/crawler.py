781d1932f50c4fbf3113bd2e57951e22a4305c42
Oalvay <s2041499@ed.ac.uk>
2020-12-20 22:12:33 +0000

    rewrite demo crawler using genius api, add gitignore file

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,73 +1,59 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 20 02:37:31 2020
-
-@author: 78182
+@author: oalvay, 78182
 """
 
-import os.path
-from os import path
-from urllib.request import Request, urlopen
+from multiprocessing import Pool, Manager
+import requests, re, time, pickle
 from bs4 import BeautifulSoup
-import csv
-from tqdm import tqdm
-from fp.fp import FreeProxy
-import requests
-class Crawler:
-    def __init__(self):
-        self.list_of_websites = ['azlyrics.com']
-        self.print_info()
-        
-    def print_info(self):
-        print('Directory for crawled lyrics {} \n'.format(os.path.realpath('..')))
-        print('Crawling from {} \n'.format(self.list_of_websites))
-        for w in self.list_of_websites:
-            if(path.exists(os.path.realpath('..')+'\\{}'.format(w+'.csv'))):
-                print(w + 'is crawled \n')
-            else:
-                print(w + 'is not yet crawled, started crawling \n')
-                self.start_crawling(w)
-    
-    def start_crawling(self,website):
-        if(website == 'azlyrics.com'):
-            
-            print('start crawling from {}'.format(website))
-            author_song_lyrics_dict = {}
-            
-            appendix = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','19']
-            proxy = FreeProxy(country_id=['UK'],rand=True).get()
-            for appen in tqdm(appendix,desc='downloading author categories by initial'):
-                url = 'http://'+website+'/'+appen+'.html'
-                
-                req = Request(url,headers = { 'User-Agent' : 'Mozilla/5.0' },proxy=proxy)
-                soup = BeautifulSoup(req.text, "lxml")
-                author_hrefs = soup.find_all("div",{'class':'col-sm-6 text-center artist-col'})
-                hrefs_list = []
-                for href in author_hrefs:
-                   hrefs_list =  hrefs_list + href.find_all('a')
-                  
-                links_list = []
-                author_list =[]
-                for h in hrefs_list:
-                    links_list.append(h.get('href'))
-                    author_list.append(h.text)
 
-                for author,link in zip(author_list,links_list):
-                    proxy = FreeProxy(country_id=['UK'],rand=True).get()
-                    author_url = 'http://'+website+'/'+link
-                    req = Request(author_url,headers = { 'User-Agent' : 'Mozilla/5.0'},proxy=proxy)
-                    soup = BeautifulSoup(req.text, "lxml")
-                    song_hrefs = soup.find_all("div",{'class':'listalbum-item'})
-                    song_titles = []
-                    song_lyrics = []
-                    proxy = FreeProxy(country_id=['UK'],rand=True).get()
-                    for h in song_hrefs:
-                        song_titles.append(h.text)
-                        lyric_link = 'http://'+website + h.find('a')['href'][2:]
-                        req = Request(lyric_link,headers = { 'User-Agent' : 'Mozilla/5.0'},proxy= proxy)
-                        soup = BeautifulSoup(req.text, "lxml")
-                        lyrics = soup.find_all("div",class_=None)[1]
-                        lyrics_contents = lyrics.contents[2:]
-                        song_lyrics.append(lyrics_contents)
-                    for s,l in zip(song_titles,song_lyrics):
-                        author_song_lyrics_dict[(author,s)] = l
+headers = {'Authorization':
+           'Bearer NxudGFdc5dNGFgFn07XO9BMe7Gz0k6wAtQ9PkvX1dQC9FduLKMJYL7gnKLyZrQpf'}
+
+def get_song(song_id):
+    example = requests.get(f"https://api.genius.com/songs/{song_id}",
+                                     headers=headers).json()
+    print('code:', example['meta']['status'], ', id:',song_id)
+    if example['meta']['status'] != 200:
+        return None
+    song = example['response']['song']
+        
+    artist = song['primary_artist']['name']
+    if(song['album'] == None):
+        album_name = 'No Album'
+    else:
+        album_name = song['album']['name']
+    song_name = song['title']
+
+    song_path = example['response']['song']['path']
+    html = BeautifulSoup(
+        requests.get(f"https://genius.com{song_path}", headers=headers).text,
+        "html.parser"
+    )
+
+    # fork from https://github.com/johnwmillr/LyricsGenius
+    # Determine the class of the div
+    div = html.find("div", class_=re.compile("^lyrics$|Lyrics__Root"))
+    lyrics = div.get_text(" ")
+    
+    return song_id, artist, album_name, song_name, lyrics
+    
+manager = Manager()
+global managed_l
+managed_l= manager.list()
+
+def call_api(song_id):
+    t = get_song(song_id)
+    if t != None:
+        managed_l.append(t)
+        
+if __name__ == '__main__':
+    start = time.time()
+    
+    with Pool(16) as p:
+        p.map(call_api,range(1000))
+    with open('somefile', 'wb') as f:
+        pickle.dump(list(managed_l), f)
+        
+    duration = time.time() - start
+    print("time used:", duration)
