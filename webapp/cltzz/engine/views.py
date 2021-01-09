f729aad8d9024a172406ec555b7a70a291afbb4a
Oalvay <s2041499@ed.ac.uk>
2021-01-09 17:50:53 +0000

    create django app

---
+++ webapp/cltzz/engine/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def index(request):
+    return render(request, 'engine/index.html')
+
+def result(request):
+    return render(request, 'engine/result.html')
+
+def detail(request):
+    return render(request, 'engine/detail.html')
